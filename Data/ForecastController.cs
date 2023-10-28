a1ba1c69ad13bb403f07603f258808312fb39c3a
Emilia Brzezinska <emilia.brzezinska@gmail.com>
2023-10-28 21:21:47 +0200

    connection failed  with the server via http client and get

---
+++ Data/ForecastController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+//amespace FirstBlazorApp1.Data
+
+//{
+  //  [Route("api/[controller]")]
+  //  [ApiController]
+    
+  //  public class ForecastController : ControllerBase
+ //   {
+    //    [HttpGet]
+   //     public async Task<ActionResult<IEnumerable<WeatherForecast>>> GetForecastAsync()
+  //      {
+          //  var svc = new WeatherForecastService();
+
+         //   return new OkObjectResult(await svc.GetForecastAsync(DateTime.Now));
+        //}
+            
+            
+       
+    
+    //}
+    
+//}
+
--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,5 +28,7 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.MapControllers();
 
 app.Run();
